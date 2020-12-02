c288cdbbe281fc3369c09925a0747741691e20b4
drkphnx137 <45473335+drkphnx137@users.noreply.github.com>
2020-12-01 19:26:25 -0500

    Chapter 2.  Working on countBs.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 /*** CHAPTER 2 - PROGRAM STRUCTURE ***/
 
-
-
 function chessboard(n) {
     let string = '';
 
@@ -17,4 +15,40 @@ function chessboard(n) {
     }
 
     console.log(string);   
+}
+
+/*** CHAPTER 3 - FUNCTIONS ***/
+
+function mathMin(n1, n2) {
+    if(n1 < n2) {
+        return n1;
+    } else {
+        return n2;
+    }
+}
+
+function isEven(n) {
+    if(n > 1) {
+        return isEven(n - 2);
+    } else if(n === 1) {
+        return 'odd';
+    } else if (n === 0) {
+        return ' even';
+    } else if (n < 0) {
+        return isEven(n - n - n);
+    }
+}
+
+function countsBs(word) {
+    let counter = 0;
+    let letter;
+
+    for(let i = 0; i < word.length; i++) {
+        letter = word[i];
+        if(letter.toLowerCase === 'b') {
+            counter++;
+        } else {
+            counter += 0;
+        }
+    }
 }
