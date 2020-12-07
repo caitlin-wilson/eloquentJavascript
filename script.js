580960aa33a8de4d15549380d95fe8e5c7f0fc27
drkphnx137 <45473335+drkphnx137@users.noreply.github.com>
2020-12-07 09:45:13 -0500

    Finished Ch. 3.   **Revisit last two examples.
    At some point, I have to go back and do the aList problem and revisit the solution to deepEqual.  Not sure I quite understand it yet.

--- script.js
+++ script.js
@@ -65,14 +65,81 @@ function countLetter(word, letter) {
 
 /*** CHAPTER 3 - DATA STRUCTURES ***/
 
-function range(lowN, highN) {
+function range(lowN, highN, step) {
     let rangeArr = [];
     
-    while(lowN <= highN) {
-        rangeArr.push(lowN);
-        lowN++;
+    if(lowN < highN) {
+        while(lowN <= highN) {
+            rangeArr.push(lowN);
+            if(step > 0) {
+                lowN = lowN + step;
+            } else {
+                lowN++;
+            }
+        }
+    } else if (lowN > highN) {
+        while(lowN >= highN) {
+            rangeArr.push(lowN);
+            if(step < 0) {
+                lowN = lowN + step;
+            } else {
+                lowN--;
+            }
+        }
     }
-
     return rangeArr;
 }
 
+function reverseArray(array) {
+    let newArr = [];
+
+    for(let i = array.length; i > -1; i--) {
+        newArr.push(array[i]);
+    }
+
+    return newArr;
+}
+
+function reverseArrayInPlace(array) {
+    const length = array.length;
+    for(let i = length; i > -1; i--) {
+        array.push(array[i]);
+    }
+        
+    // for(let j = 0; j<length + 1; j++) {
+    //     array.shift();
+    // }
+
+    return array;
+}
+
+//COME BACK TO function aList(){...}
+
+function deepEqual(val1, val2) {
+    if(val1 === val2) {
+        return true;
+    }
+    if(typeof val1 != 'object' || typeof val1 == 'null' 
+    || typeof val2 != 'object' || typeof val2 == 'null') {
+        return false;
+    }
+
+    let props1 = 0;
+    let props2 = 0;
+
+    for(var prop in val1) {
+        props1++;
+    }
+
+    for(prop in val2) {
+        props2++;
+        
+        if(!(prop in val2) || !deepEqual(val1[prop], val2[prop])) {
+            return false;
+        }
+    }
+
+    return props1 == props2;
+}
+
+let obj = {here: {is: "an"}, object: 2};    
