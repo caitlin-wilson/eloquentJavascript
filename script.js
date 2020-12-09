d0f11d5d736bd1d8a05ef627ce7a2439a943b76f
drkphnx137 <45473335+drkphnx137@users.noreply.github.com>
2020-12-08 19:58:23 -0500

    1st problem from Ch 5.

--- script.js
+++ script.js
@@ -1,145 +1,154 @@
 /*** CHAPTER 2 - PROGRAM STRUCTURE ***/
 
 function chessboard(n) {
-    let string = '';
+  let string = '';
 
-    for(let i = 0; i < n; i++) {
-        for(j = 0; j < n; j++) {
-            if(string.length % 2 === 0) {
-                string += '#';
-            } else {
-                string += ' ';
-            }
-        }
-        string = string + '\n';
-    }
+  for(let i = 0; i < n; i++) {
+      for(j = 0; j < n; j++) {
+          if(string.length % 2 === 0) {
+              string += '#';
+          } else {
+              string += ' ';
+          }
+      }
+      string = string + '\n';
+  }
 
-    console.log(string);   
+  console.log(string);   
 }
 
 /*** CHAPTER 3 - FUNCTIONS ***/
 
 function mathMin(n1, n2) {
-    if(n1 < n2) {
-        return n1;
-    } else {
-        return n2;
-    }
+  if(n1 < n2) {
+      return n1;
+  } else {
+      return n2;
+  }
 }
 
 function isEven(n) {
-    if(n > 1) {
-        return isEven(n - 2);
-    } else if(n === 1) {
-        return 'odd';
-    } else if (n === 0) {
-        return ' even';
-    } else if (n < 0) {
-        return isEven(n - n - n);
-    }
+  if(n > 1) {
+      return isEven(n - 2);
+  } else if(n === 1) {
+      return 'odd';
+  } else if (n === 0) {
+      return ' even';
+  } else if (n < 0) {
+      return isEven(n - n - n);
+  }
 }
 
 function countsBs(word) {
-    let counter = 0;
+  let counter = 0;
 
-    for(let i = 0; i < word.length; i++) {
-        if(word[i].toLowerCase() === 'b') {
-            counter++;
-        }
-    }
+  for(let i = 0; i < word.length; i++) {
+      if(word[i].toLowerCase() === 'b') {
+          counter++;
+      }
+  }
 
-    return counter;
+  return counter;
 }
 
 function countLetter(word, letter) {
-    let counter = 0;
+  let counter = 0;
 
-    for(let i = 0; i < word.length; i++) {
-        if(word[i].toLowerCase() === letter) {
-            counter++;
-        }
-    }
+  for(let i = 0; i < word.length; i++) {
+      if(word[i].toLowerCase() === letter) {
+          counter++;
+      }
+  }
 
-    return counter;
+  return counter;
 }
 
 /*** CHAPTER 3 - DATA STRUCTURES ***/
 
 function range(lowN, highN, step) {
-    let rangeArr = [];
-    
-    if(lowN < highN) {
-        while(lowN <= highN) {
-            rangeArr.push(lowN);
-            if(step > 0) {
-                lowN = lowN + step;
-            } else {
-                lowN++;
-            }
-        }
-    } else if (lowN > highN) {
-        while(lowN >= highN) {
-            rangeArr.push(lowN);
-            if(step < 0) {
-                lowN = lowN + step;
-            } else {
-                lowN--;
-            }
-        }
-    }
-    return rangeArr;
+  let rangeArr = [];
+  
+  if(lowN < highN) {
+      while(lowN <= highN) {
+          rangeArr.push(lowN);
+          if(step > 0) {
+              lowN = lowN + step;
+          } else {
+              lowN++;
+          }
+      }
+  } else if (lowN > highN) {
+      while(lowN >= highN) {
+          rangeArr.push(lowN);
+          if(step < 0) {
+              lowN = lowN + step;
+          } else {
+              lowN--;
+          }
+      }
+  }
+  return rangeArr;
 }
 
 function reverseArray(array) {
-    let newArr = [];
+  let newArr = [];
 
-    for(let i = array.length; i > -1; i--) {
-        newArr.push(array[i]);
-    }
+  for(let i = array.length; i > -1; i--) {
+      newArr.push(array[i]);
+  }
 
-    return newArr;
+  return newArr;
 }
 
 function reverseArrayInPlace(array) {
-    const length = array.length;
-    for(let i = length; i > -1; i--) {
-        array.push(array[i]);
-    }
-        
-    // for(let j = 0; j<length + 1; j++) {
-    //     array.shift();
-    // }
+  const length = array.length;
+  for(let i = length; i > -1; i--) {
+      array.push(array[i]);
+  }
+      
+  // for(let j = 0; j<length + 1; j++) {
+  //     array.shift();
+  // }
 
-    return array;
+  return array;
 }
 
 //COME BACK TO function aList(){...}
 
 function deepEqual(val1, val2) {
-    if(val1 === val2) {
-        return true;
-    }
-    if(typeof val1 != 'object' || typeof val1 == 'null' 
-    || typeof val2 != 'object' || typeof val2 == 'null') {
-        return false;
-    }
+  if(val1 === val2) {
+      return true;
+  }
+  if(typeof val1 != 'object' || typeof val1 == 'null' 
+  || typeof val2 != 'object' || typeof val2 == 'null') {
+      return false;
+  }
 
-    let props1 = 0;
-    let props2 = 0;
+  let props1 = 0;
+  let props2 = 0;
 
-    for(var prop in val1) {
-        props1++;
-    }
+  for(var prop in val1) {
+      props1++;
+  }
 
-    for(prop in val2) {
-        props2++;
-        
-        if(!(prop in val2) || !deepEqual(val1[prop], val2[prop])) {
-            return false;
-        }
-    }
+  for(prop in val2) {
+      props2++;
+      
+      if(!(prop in val2) || !deepEqual(val1[prop], val2[prop])) {
+          return false;
+      }
+  }
 
-    return props1 == props2;
+  return props1 == props2;
 }
 
 let obj = {here: {is: "an"}, object: 2};    
+
+/*** CHAPTER 5 - HIGHER-ORDER FUNCTIONS ***/
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+let flattening = arrays.reduce((acc, val) => {
+  return acc.concat(val);
+}, []) 
+
